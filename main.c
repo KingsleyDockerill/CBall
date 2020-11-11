0ab3f04246b91d249ea6654da35254cdf0c5c77b
MrWhatsHisFace <Kingsmo2008@outlook.com>
2020-11-11 15:11:09 +0000

    Still fixing errors

--- main.c
+++ main.c
@@ -7,7 +7,9 @@ int main(void) {
   while(1){
     printf(">>> ");
     token* tokens = generate_tokens();
-    free(tokens);
+    for(int i = 0; tokens[i].value != NULL; i++){
+      printf("%s:%s\n", tokens[i].name, tokens[i].value);
+    }
   }
   return 0;
 }
